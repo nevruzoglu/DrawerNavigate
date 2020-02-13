5bfcb4cc6874a5f9a384fe81e7615c2f88e1e60d
nevruzoglu <nevruzoglu@gmail.com>
2020-02-14 00:29:18 +0300

    added swipte to remove listtile

--- lib/new_page.dart
+++ lib/new_page.dart
@@ -46,7 +46,7 @@ class NewPage extends StatelessWidget {
   }
 }
 
-class ShipCard extends StatelessWidget {
+class ShipCard<ListTile> extends StatelessWidget {
   final String shipPic;
   final String flagPic;
   final String shipName;
@@ -56,43 +56,55 @@ class ShipCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Image.asset(shipPic),
-        Positioned(
-          top: 16,
-          right: 16,
-          child: Icon(Icons.favorite_border, color: Colors.white),
-          height: 16,
-          width: 32,
+    return Dismissible(
+      //! Swipe Right to delete
+      key: ValueKey(Stack),
+      background: Container(
+        color: Colors.red,
+        child: Icon(
+          Icons.transit_enterexit,
+          size: 40,
+          color: Colors.white,
         ),
-        Positioned(
-            bottom: 18,
-            left: 16,
-            child: Container(
-              height: 16,
-              width: 32,
-              child: Image.asset(flagPic),
-            )),
-        Positioned(
-            bottom: 16,
-            left: 58,
-            child: Text(
-              shipName,
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            )),
-        Positioned(
-            bottom: 16,
+      ),
+      child: Stack(
+        children: <Widget>[
+          Image.asset(shipPic),
+          Positioned(
+            top: 16,
             right: 16,
-            child: Text(
-              shipPrice,
-              style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600),
-            ))
-      ],
+            child: Icon(Icons.favorite_border, color: Colors.white),
+            height: 16,
+            width: 32,
+          ),
+          Positioned(
+              bottom: 18,
+              left: 16,
+              child: Container(
+                height: 16,
+                width: 32,
+                child: Image.asset(flagPic),
+              )),
+          Positioned(
+              bottom: 16,
+              left: 58,
+              child: Text(
+                shipName,
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              )),
+          Positioned(
+              bottom: 16,
+              right: 16,
+              child: Text(
+                shipPrice,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600),
+              ))
+        ],
+      ),
     );
   }
 }

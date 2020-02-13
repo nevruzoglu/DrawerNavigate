ee449a47b7a230c5c8e4f6d295872bef6fd15950
nevruzoglu <nevruzoglu@gmail.com>
2020-02-13 22:44:15 +0300

    refactor code

--- lib/new_page.dart
+++ lib/new_page.dart
@@ -14,159 +14,85 @@ class NewPage extends StatelessWidget {
         child: Container(
           child: ListView(
             children: <Widget>[
-              Stack(
-                children: <Widget>[
-                  Image.asset('assets/1boat.png'),
-                  Positioned(
-                    top: 16,
-                    right: 16,
-                    child: Icon(Icons.favorite_border, color: Colors.white),
-                    height: 16,
-                    width: 32,
-                  ),
-                  Positioned(
-                      bottom: 18,
-                      left: 16,
-                      child: Container(
-                        height: 16,
-                        width: 32,
-                        child: Image.asset('assets/tr.png'),
-                      )),
-                  Positioned(
-                      bottom: 16,
-                      left: 58,
-                      child: Text(
-                        'Sun Odysey',
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      )),
-                  Positioned(
-                      bottom: 16,
-                      right: 16,
-                      child: Text(
-                        '\$100.000.000',
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600),
-                      ))
-                ],
+              ShipCard(
+                shipName: 'Odyseyy',
+                shipPic: 'assets/1boat.png',
+                shipPrice: '\$100.000.000',
+                flagPic: 'assets/tr.png',
               ),
-              Stack(
-                children: <Widget>[
-                  Image.asset('assets/2boat.png'),
-                  Positioned(
-                    top: 16,
-                    right: 16,
-                    child: Icon(Icons.favorite_border, color: Colors.white),
-                    height: 16,
-                    width: 32,
-                  ),
-                  Positioned(
-                    bottom: 18,
-                    left: 16,
-                    child: Image.asset('assets/ca.png'),
-                    height: 16,
-                    width: 32,
-                  ),
-                  Positioned(
-                      bottom: 16,
-                      left: 58,
-                      child: Text(
-                        'Mad Duster',
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      )),
-                  Positioned(
-                      bottom: 16,
-                      right: 16,
-                      child: Text(
-                        '\$10.000.000',
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600),
-                      ))
-                ],
+              ShipCard(
+                shipName: 'Levinski',
+                shipPic: 'assets/2boat.png',
+                shipPrice: '\$45.000.000',
+                flagPic: 'assets/ca.png',
               ),
-              Stack(
-                children: <Widget>[
-                  Image.asset('assets/3boat.png'),
-                  Positioned(
-                    top: 16,
-                    right: 16,
-                    child: Icon(Icons.favorite_border, color: Colors.white),
-                    height: 16,
-                    width: 32,
-                  ),
-                  Positioned(
-                    bottom: 18,
-                    left: 16,
-                    child: Image.asset('assets/fr.png'),
-                    height: 16,
-                    width: 32,
-                  ),
-                  Positioned(
-                      bottom: 16,
-                      left: 58,
-                      child: Text(
-                        'Moon Rider',
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      )),
-                  Positioned(
-                      bottom: 16,
-                      right: 16,
-                      child: Text(
-                        '\$22.000.000',
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600),
-                      ))
-                ],
+              ShipCard(
+                shipName: 'Moramati Folkart',
+                shipPic: 'assets/3boat.png',
+                shipPrice: '\$10.000.000',
+                flagPic: 'assets/tr.png',
               ),
-              Stack(
-                children: <Widget>[
-                  Image.asset('assets/4boat.png'),
-                  Positioned(
-                    top: 16,
-                    right: 16,
-                    child: Icon(Icons.favorite_border, color: Colors.white),
-                    height: 16,
-                    width: 32,
-                  ),
-                  Positioned(
-                    bottom: 18,
-                    left: 16,
-                    child: Image.asset('assets/ca.png'),
-                    height: 16,
-                    width: 32,
-                  ),
-                  Positioned(
-                      bottom: 16,
-                      left: 58,
-                      child: Text(
-                        'Deep Blur',
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      )),
-                  Positioned(
-                      bottom: 16,
-                      right: 16,
-                      child: Text(
-                        '\$260.000.000',
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600),
-                      ))
-                ],
+              ShipCard(
+                shipName: 'Moon Rider',
+                shipPic: 'assets/4boat.png',
+                shipPrice: '\$200.000.000',
+                flagPic: 'assets/fr.png',
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class ShipCard extends StatelessWidget {
+  final String shipPic;
+  final String flagPic;
+  final String shipName;
+  final String shipPrice;
+
+  ShipCard({this.shipName, this.shipPrice, this.shipPic, this.flagPic});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(shipPic),
+        Positioned(
+          top: 16,
+          right: 16,
+          child: Icon(Icons.favorite_border, color: Colors.white),
+          height: 16,
+          width: 32,
+        ),
+        Positioned(
+            bottom: 18,
+            left: 16,
+            child: Container(
+              height: 16,
+              width: 32,
+              child: Image.asset(flagPic),
+            )),
+        Positioned(
+            bottom: 16,
+            left: 58,
+            child: Text(
+              shipName,
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            )),
+        Positioned(
+            bottom: 16,
+            right: 16,
+            child: Text(
+              shipPrice,
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600),
+            ))
+      ],
     );
   }
 }
